b2a9b2f9719defa688567a7dd1de0cb76da72fbc
ABHISHEK SUDHIR <abhisheksudhir2000@gmail.com>
2020-06-07 20:54:20 +0530

    styling & theming

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -13,29 +13,32 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridTile(
-      child: Image.network(
-        imageUrl,
-        fit: BoxFit.cover, //to take up all the available space it can get
-      ),
-      //footer adds a bar at the bottom of the gridtile
-      footer: GridTileBar(
-        backgroundColor: Colors.black54,
-        leading: IconButton(
-          icon: Icon(Icons.favorite),
-          onPressed: () {
-            
-          },
+    // ClipRRect uses its own bounds as the base rectangle for the clip,
+    // but the size and location of the clip can be customized using a custom clipper.
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        child: Image.network(
+          imageUrl,
+          fit: BoxFit.cover, //to take up all the available space it can get
         ),
-        title: Text(
-          title,
-          textAlign: TextAlign.center,
-        ),
-        trailing: IconButton(
-          icon: Icon(Icons.shopping_cart),
-          onPressed: () {
-            
-          },
+        //footer adds a bar at the bottom of the gridtile
+        footer: GridTileBar(
+          backgroundColor: Colors.black87,
+          leading: IconButton(
+            icon: Icon(Icons.favorite),
+            color: Theme.of(context).accentColor,
+            onPressed: () {},
+          ),
+          title: Text(
+            title,
+            textAlign: TextAlign.center,
+          ),
+          trailing: IconButton(
+            icon: Icon(Icons.shopping_cart),
+            color: Theme.of(context).accentColor,
+            onPressed: () {},
+          ),
         ),
       ),
     );
