e939981eb4019cfc59171cc1f2fde1554907418f
ABHISHEK SUDHIR <abhisheksudhir2000@gmail.com>
2020-06-08 16:07:54 +0530

    adding product detail data

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -17,11 +17,50 @@ class ProductDetailScreen extends StatelessWidget {
         as String; //extracting the arguments(id) as string
     final loadedProduct = Provider.of<Products>(
       context,
-      listen: false,  //is default true. is put as false as we want to get data only once. widget should not rerun on every update
+      listen:
+          false, //is default true. is put as false as we want to get data only once. widget should not rerun on every update
     ).findById(productId);
     return Scaffold(
       appBar: AppBar(
         title: Text(loadedProduct.title),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(
+                loadedProduct.imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              '\$ ${loadedProduct.price}',
+              style: TextStyle(
+                color: Colors.grey,
+                fontSize: 20,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(
+                horizontal: 10,
+              ),
+              width: double.infinity,
+              child: Text(
+                loadedProduct.description,
+                textAlign: TextAlign.center,
+                softWrap: true,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
