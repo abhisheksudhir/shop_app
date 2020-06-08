5441d45fecd9de30c3dadd24289f63a7aef5a730
ABHISHEK SUDHIR <abhisheksudhir2000@gmail.com>
2020-06-08 11:22:54 +0530

    listening in different places & ways

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/products.dart';
 
 class ProductDetailScreen extends StatelessWidget {
   // final String title;
@@ -10,10 +13,15 @@ class ProductDetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final productId = ModalRoute.of(context).settings.arguments as String;  //extracting the arguments(id) as string
+    final productId = ModalRoute.of(context).settings.arguments
+        as String; //extracting the arguments(id) as string
+    final loadedProduct = Provider.of<Products>(
+      context,
+      listen: false,  //is default true. is put as false as we want to get data only once. widget should not rerun on every update
+    ).findById(productId);
     return Scaffold(
       appBar: AppBar(
-        title: Text('title'),
+        title: Text(loadedProduct.title),
       ),
     );
   }
