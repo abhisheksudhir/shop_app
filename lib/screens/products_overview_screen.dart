0a005a0e77d7c9c4d0b4cd59d1fe6bd62c039649
ABHISHEK SUDHIR <abhisheksudhir2000@gmail.com>
2020-06-08 12:41:54 +0530

    local state vs app-wide state

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import '../providers/products.dart';
 import '../widgets/products_grid.dart';
 
-class ProductsOverviewScreen extends StatelessWidget {
+enum FilterOptions {
+  Favorites,
+  All,
+}
+
+class ProductsOverviewScreen extends StatefulWidget {
+  @override
+  _ProductsOverviewScreenState createState() => _ProductsOverviewScreenState();
+}
+
+class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
+  var _showOnlyFavorites = false;
+
   @override
   Widget build(BuildContext context) {
+    // final productsContainer = Provider.of<Products>(
+    //   context,
+    //   listen: false,
+    // );
+
     // Scaffold will expand or occupy in the whole device screen.
     // It will occupy the available space.
     // Scaffold will provide a framework to implement the basic material design layout of the application.
@@ -13,10 +32,36 @@ class ProductsOverviewScreen extends StatelessWidget {
         title: Text(
           'MyShop',
         ),
+        actions: <Widget>[
+          PopupMenuButton(
+            onSelected: (FilterOptions selectedValue) {
+              setState(() {
+                if (selectedValue == FilterOptions.Favorites) {
+                  // productsContainer.showFavoritesOnly();
+                  _showOnlyFavorites = true;
+                } else {
+                  // productsContainer.showAll();
+                  _showOnlyFavorites = false;
+                }
+              });
+            },
+            icon: Icon(
+              Icons.more_vert,
+            ),
+            itemBuilder: (_) => [
+              PopupMenuItem(
+                child: Text('Only Favorites'),
+                value: FilterOptions.Favorites,
+              ),
+              PopupMenuItem(
+                child: Text('Show All'),
+                value: FilterOptions.All,
+              ),
+            ],
+          ),
+        ],
       ),
-      body: ProductsGrid(),
+      body: ProductsGrid(_showOnlyFavorites),
     );
   }
 }
-
-
