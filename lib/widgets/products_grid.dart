c389c3f8a3eead1e78d2dafacc1c10a61a7ed344
ABHISHEK SUDHIR <abhisheksudhir2000@gmail.com>
2020-06-08 11:56:34 +0530

    exploring alternative provider syntaxes

--- lib/widgets/products_grid.dart
+++ lib/widgets/products_grid.dart
@@ -13,8 +13,10 @@ class ProductsGrid extends StatelessWidget {
     return GridView.builder(
       padding: const EdgeInsets.all(10.0),
       itemCount: products.length,
-      itemBuilder: (ctx, i) => ChangeNotifierProvider(
-        create: (c) => products[i],
+      //ChangeNotifierProvider.value should be used with lists and grids where flutter recycles the widgets
+      itemBuilder: (ctx, i) => ChangeNotifierProvider.value(
+        // create: (c) => products[i],
+        value: products[i],
         child: ProductItem(
           // products[i].id,
           // products[i].title,
