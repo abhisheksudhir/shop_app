e14bb099ce28fe24d92297dba3b1a0b46b1423a7
ABHISHEK SUDHIR <abhisheksudhir2000@gmail.com>
2020-06-08 16:35:59 +0530

    providing an orders object

---
+++ lib/providers/orders.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/cupertino.dart';
+
+import './cart.dart';
+
+class OrderItem {
+  final String id;
+  final double amount;
+  final List<CartItem> products;
+  final DateTime dateTime;
+
+  OrderItem({
+    @required this.id,
+    @required this.amount,
+    @required this.products,
+    @required this.dateTime,
+  });
+}
+
+//mix in are added using with keyword.You merge some properties into you existing class
+//you don't return your class into an instance of inherited class
+class Orders with ChangeNotifier {
+  List<OrderItem> _orders = [];
+
+  List<OrderItem> get orders {
+    return [..._orders];
+  }
+
+  void addOrder(
+    List<CartItem> cartProducts,
+    double total,
+  ) {
+    _orders.insert(
+      0,
+      OrderItem(
+        id: DateTime.now().toString(),
+        amount: total,
+        products: cartProducts,
+        dateTime: DateTime.now(),
+      ),
+    );
+    notifyListeners();
+  }
+}
