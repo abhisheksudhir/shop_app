816197db53127296978a0c67cc34d49af1bf0218
ABHISHEK SUDHIR <abhisheksudhir2000@gmail.com>
2020-06-14 15:23:42 +0530

    adding a hero transition

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -49,10 +49,13 @@ class ProductItem extends StatelessWidget {
             //   ),
             // );
           },
-          child: FadeInImage(
-            placeholder: AssetImage('assets/images/product-placeholder.png'),
-            image: NetworkImage(product.imageUrl),
-            fit: BoxFit.cover,
+          child: Hero(
+            tag: product.id,
+            child: FadeInImage(
+              placeholder: AssetImage('assets/images/product-placeholder.png'),
+              image: NetworkImage(product.imageUrl),
+              fit: BoxFit.cover,
+            ),
           ),
         ),
         //footer adds a bar at the bottom of the gridtile
